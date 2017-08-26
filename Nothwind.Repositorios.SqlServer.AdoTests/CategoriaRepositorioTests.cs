dcf60f2c869c926e9be31efa56b35dfda008bb8f
sa3512 <sa3512@4P35_12>
2017-08-26 13:26:31 -0300

    Add project files.

---
+++ Nothwind.Repositorios.SqlServer.AdoTests/CategoriaRepositorioTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Data;
+
+namespace Nothwind.Repositorios.SqlServer.Ado.Tests
+{
+    [TestClass()]
+    public class CategoriaRepositorioTests
+    {
+        [TestMethod()]
+        public void SelecionarTest()
+        {
+            var categoriaRepositorio = new CategoriaRepositorio();
+
+            var categoriaDataTable = categoriaRepositorio.Selecionar();
+
+            //
+
+
+            foreach (DataRow registro in categoriaDataTable.Rows)
+            {
+                Console.WriteLine($"{registro[0]} - {registro["CategoryName"]} ");
+            }
+        }
+    }
+}
